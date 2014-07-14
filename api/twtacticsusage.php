88be5ef4da917d201e9c082d898b4940abb379bf
Laoujin <woutervs@hotmail.com>
2014-07-15 01:08:26 +0200

    TWTactics fills table with world/server/player on download twsnapshot

---
+++ api/twtacticsusage.php
@@ -0,0 +1,16 @@
+<?php
+$mysqli = new mysqli("localhost", "laoujin_laoujin", "KeepingTrackOfTheDownloads", "laoujin_twtactics");
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+else
+{
+	if ($stmt = $mysqli->prepare("insert into twsnapshotdownloads (server, world, player, tribe, ip) values (?, ?, ?, ?, ?)"))
+	{
+		$stmt -> bind_param("sssss", $_POST['server'], $_POST['world'], $_POST['player'], $_POST['tribe'], $_SERVER['REMOTE_ADDR']);
+		$stmt -> execute();
+	}
+
+	$mysqli -> close();
+}
+?>
