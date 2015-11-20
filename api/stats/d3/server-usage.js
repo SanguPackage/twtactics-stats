fc47109dd3a68fa98c3b1755172ced3b997086fa
Laoujin <woutervs@hotmail.com>
2015-11-20 02:36:51 +0100

    bar diagram works :)

--- api/stats/d3/server-usage.js
+++ api/stats/d3/server-usage.js
@@ -1,7 +1,9 @@
 var serverUsage = function(rawData) {
   var data = _.countBy(rawData, v => v.server.replace('www.', ''));
   var maxValue = _.max(_.values(data));
-  data = _.sortBy(_.pairs(data), d => -d[1]);
+  data = _.pairs(data);
+  data = _.filter(data, d => d[1] > 30);
+  data = _.sortBy(data, d => -d[1]);
 
   //console.log(data);
   //console.log(maxValue);
@@ -12,29 +14,46 @@ var serverUsage = function(rawData) {
   var y = d3.scale.linear()
       .range([height, 0]);
 
-  var chart = d3.select("#server-usage")
-    .attr("width", width)
-    .attr("height", height);
+  var chart = d3.select('#server-usage')
+    .attr('width', width)
+    .attr('height', height + 150);
 
-  y.domain([0, d3.max(data, function(d) { return d[1]; })]);
+  y.domain([0, d3.max(data, d => d[1])]);
 
   var barWidth = width / data.length;
 
-  var bar = chart.selectAll("g")
-      .data(data)
-    .enter().append("g")
-      .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; });
+  var bar = chart.selectAll('g')
+    .data(data)
+    .enter()
+      .append('g')
+      .attr('transform', (d, i) => 'translate(' + i * barWidth + ',0)');
 
-  bar.append("rect")
-      .attr("y", function(d) { return y(d[1]); })
-      .attr("height", function(d) { return height - y(d[1]); })
-      .attr("width", barWidth - 1);
+  bar.append('rect')
+    .attr('y', d => y(d[1]))
+    .attr('height', d => height - y(d[1]))
+    .attr('width', barWidth - 1);
 
-  bar.append("text")
-      .attr("x", barWidth / 2)
-      .attr("y", function(d) { return y(d[1]) + 3; })
-      .attr("dy", ".75em")
-      .text(function(d) { return d[1]; });
+  bar.append('text')
+    .attr('class', 'amount')
+    .attr('x', barWidth / 2)
+    .attr('y', d => y(d[1]) + 3)
+    .attr('dy', '.75em')
+    .text(d => d[1]);
+
+  bar.append('text')
+    .attr('x', barWidth / 2)
+    .attr('y', d => height)
+    .attr('dy', '.75em')
+    .style('writing-mode', 'tb')
+    .style('text-anchor', 'start')
+    .text(d => d[0]);
+
+  // bar.append('text')
+  //   .attr('transform', 'rotate(-90)')
+  //   .attr('y', height - 100)
+  //   //.attr('dy', '.71em')
+  //   .style('text-anchor', 'end')
+  //   .text(d => d[0]);
 
   // var canvas = d3.select('#content')
   //   .append('div')
